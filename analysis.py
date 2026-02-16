a707797ab293aaec4f40c2f3394891aac00a6a90
Alviya Laela Lestari <127363031+alviyalaela@users.noreply.github.com>
2026-02-16 19:40:42 +0700

    Delete analysis.py

--- analysis.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, initialize_app
-import pandas as pd
-
-cred = credentials.Certificate("key.json")
-firebase_admin.initialize_app(cred)
-
-
